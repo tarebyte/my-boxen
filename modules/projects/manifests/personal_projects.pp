e45e632018218a536be65e2d760800355c610e42
Mark Tareshawty <tarebyte@gmail.com>
2014-04-20 21:28:51 -0400

    removed project

--- modules/projects/manifests/personal_projects.pp
+++ modules/projects/manifests/personal_projects.pp
@@ -1,10 +1,2 @@
 class projects::personal_projects {
-
-  boxen::project { 'boxen-status':
-    ruby       => '2.1.1',
-    postgresql => true,
-    nginx      => true,
-    source     => 'tarebyte/boxen-status',
-    dir        => "${boxen::config::srcdir}/tarebyte/boxen-status"
-  }
 }
