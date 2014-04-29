9b313487c1d3d7e384924e3e80788372ecb84535
Mark Tareshawty <tarebyte@gmail.com>
2014-04-28 20:35:59 -0400

    cleaned up

--- modules/people/manifests/tarebyte.pp
+++ modules/people/manifests/tarebyte.pp
@@ -1,19 +1,18 @@
 class people::tarebyte {
+  include boxen::development
+
   include people::tarebyte::apps
   include people::tarebyte::git
   include people::tarebyte::osx
-
-  include boxen::development
-
-  # Nodejs
-  class { 'nodejs::global':
-    version => 'v0.10.26'
-  }
 
   include projects::config
   include projects::clients
   include projects::cwdg
   include projects::personal_projects
+
+  class { 'nodejs::global':
+    version => 'v0.10.26'
+  }
 
   package {
     [
