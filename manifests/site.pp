1f2d48a08d75efcc22357aaa60b2d0d04b1221b9
Mark Tareshawty <tarebyte@gmail.com>
2014-05-09 09:27:03 -0400

    added ruby 2.1.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -74,6 +74,7 @@ node default {
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
 
   # For another day
   # ruby::plugin { 'rbenv-gem-rehash':
