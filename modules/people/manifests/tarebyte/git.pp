94bb88a51c628b187d14ad97884c75e0166e6982
Mark Tareshawty <tarebyte@gmail.com>
2014-07-02 01:07:00 -0400

    added git undo to git aliases

--- modules/people/manifests/tarebyte/git.pp
+++ modules/people/manifests/tarebyte/git.pp
@@ -20,6 +20,7 @@ class people::tarebyte::git {
     'alias.today'         : value => "log --since=midnight --author='Mark Tareshawty' --oneline";
     'alias.type'          : value => 'cat-file -t';
     'alias.rmb'           : value => "!sh -c 'git branch -D \$1 && git push origin :\$1' -";
+    'alias.undo'          : value => 'reset --soft HEAD^';
     'alias.up'            : value => "!sh -c 'git pull --rebase --prune && git log --pretty=format:\\\"%Cred%ae %Creset- %C(yellow)%s %Creset(%ar)\\\" HEAD@{1}..'";
     'alias.who'           : value => 'shortlog -n -s --no-merges';
 
