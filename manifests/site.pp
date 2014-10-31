01607bf6261e1d79b6dc4873ee3914c6ce18f261
Mark Tareshawty <tarebyte@gmail.com>
2014-10-31 11:47:01 -0400

    update ruby stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -69,12 +69,9 @@ node default {
   nodejs::version { 'v0.10': }
 
   # default ruby versions
-  ruby::version { '1.8.7': }
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  ruby::version { '2.1.4': }
 
   # common, useful packages
   package {
