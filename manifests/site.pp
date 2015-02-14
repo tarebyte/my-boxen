95384cf0eaba6e35a8bcf03775d688b2c7b82e6a
Mark Tareshawty <tarebyte@gmail.com>
2015-02-14 15:41:56 -0500

    added github version and removed gist_lists

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,6 +70,7 @@ node default {
 
   # default ruby versions
   ruby::version { '1.9.3': }
+  ruby::version { '1.9.3-p231-tcs-github': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0-github': }
   ruby::version { '2.1-github': }
--- modules/projects/manifests/gist_lists.pp
+++
@@ -1,9 +0,0 @@
-class projects::gist_lists {
-  boxen::project { 'gist_lists':
-    dir        => "${boxen::config::srcdir}/tarebyte/gist_lists",
-    nginx      => true,
-    postgresql => true,
-    ruby       => '2.2.0',
-    source     => 'tarebyte/gist_lists'
-  }
-}
